c58a188afb927227f35bb2afdb1a03d9be2dab3e
YUU-HANG <1398540400@qq.com>
2023-03-25 16:20:46 +0800

    用户会话拦截器

--- news-dev-service-api/src/main/java/com/zhou/api/interceptors/UserActiveInterceptor.java
+++ news-dev-service-api/src/main/java/com/zhou/api/interceptors/UserActiveInterceptor.java
@@ -1,10 +1,5 @@
 package com.zhou.api.interceptors;
 
-import com.zhou.exception.GraceException;
-import com.zhou.grace.result.ResponseStatusEnum;
-import com.zhou.utils.IPUtil;
-import com.zhou.utils.RedisOperator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,17 +8,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * @Description:
+ * @Description: 用户激活状态检查拦截器
+ * 发文章, 修改文章, 删除文章,
+ * 发表评论, 查看评论等
+ * 这些接口都需要在用户激活以后, 才能进行
+ * 否则需要提示用户前往[账号设置]去修改信息
  * @Author: yuhang
- * @Date: 2023/3/13 19:13
+ * @Date: 2023/3/25 16:01
  * @Version: 1.0
  **/
 @Component
-public class UserTokenInterceptor extends BaseInterceptor implements HandlerInterceptor {
+public class UserActiveInterceptor extends BaseInterceptor implements HandlerInterceptor {
 
     /**
      * 拦截请求，访问controller之前
-     *
      * @param request
      * @param response
      * @param handler
@@ -33,8 +31,8 @@ public class UserTokenInterceptor extends BaseInterceptor implements HandlerInte
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        String userId = request.getHeader("headerUserId");
-        String userToken = request.getHeader("headerUserToken");
+        String userId = request.getHeader("headerUerId");
+        String userToken = request.getHeader("headerUerToken");
 
         // 判断是否放行
         boolean run = verifyUserIdToken(userId, userToken, REDIS_USER_TOKEN);
@@ -51,7 +49,6 @@ public class UserTokenInterceptor extends BaseInterceptor implements HandlerInte
 
     /**
      * 请求访问到controller之后，渲染视图之前
-     *
      * @param request
      * @param response
      * @param handler
@@ -65,7 +62,6 @@ public class UserTokenInterceptor extends BaseInterceptor implements HandlerInte
 
     /**
      * 请求访问到controller之后，渲染视图之后
-     *
      * @param request
      * @param response
      * @param handler
